ef1a0fbe3118c8d386d8350db29bebd4bc5a60e9
Kais Dewani <kaisdewani@yahoo.com>
2018-11-25 14:47:14 -0500

    add description and avatar as props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-     <CommentDetail author="Sam"  timeAgo="Today at 4:45PM" />
-     <CommentDetail author="Alex" timeAgo="Today at 2:00AM" />
-     <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" />
+     <CommentDetail author="Sam"  timeAgo="Today at 4:45PM" words="nice post" avatarPhoto={faker.image.avatar()} />
+     <CommentDetail author="Alex" timeAgo="Today at 2:00AM" words="I agree" avatarPhoto={faker.image.avatar()} />
+     <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" words="YOLO" avatarPhoto={faker.image.avatar()} />
     </div>
   );
 };
