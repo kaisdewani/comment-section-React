d0ada7e51b4be0c9801f5919b5e4cfdee9646596
Kais Dewani <kaisdewani@yahoo.com>
2018-11-25 14:54:55 -0500

    better prop names

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-     <CommentDetail author="Sam"  timeAgo="Today at 4:45PM" words="nice post" avatarPhoto={faker.image.avatar()} />
-     <CommentDetail author="Alex" timeAgo="Today at 2:00AM" words="I agree" avatarPhoto={faker.image.avatar()} />
-     <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" words="YOLO" avatarPhoto={faker.image.avatar()} />
+     <CommentDetail author="Sam"  timeAgo="Today at 4:45PM"     content="nice blog post"      avatar={faker.image.avatar()} />
+     <CommentDetail author="Alex" timeAgo="Today at 2:00AM"     content="I like the subject"  avatar={faker.image.avatar()} />
+     <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" content="I like the writing"  avatar={faker.image.avatar()} />
     </div>
   );
 };
