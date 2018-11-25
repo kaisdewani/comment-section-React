b2ce33c7a16304720e58572b7daea9677f4ae788
Kais Dewani <kaisdewani@yahoo.com>
2018-11-25 14:38:22 -0500

    add jane and alex comments using props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-     <CommentDetail />
+     <CommentDetail author="Sam"  />
+     <CommentDetail author="Alex" />
+     <CommentDetail author="Jane" />
     </div>
-
   );
 };
 
